41dd6d6c9731e9cf707d8c35d13dc077293cf425
rrcarlson <Rachel.R.Carl@gmail.com>
2023-02-07 17:40:36 -0800

    Edits

--- EDA.R
+++ EDA.R
@@ -2,4 +2,24 @@
 ######### 07 February 2023
 ######### Rachel R. Carlson
 
+# Libraries
+library(tidyverse)
+library(ncdf4)
+library(raster)
+library(sf)
+library(purrr)
+library(lubridate)
+
 ### Sorting datasets for examining freshwater impact on OA in California, Washington, Oregon.
+# Filter by datasets with time series inside relevant dates
+id_list <- c(30, 31, 32, 33, 39, 40, 50, 56, 64, 66, 67, 70)
+trim <- full %>% filter(dataset_id %in% id_list)
+trim <- trim %>% group_by(dataset_id) %>% mutate(min_time = min(time_utc, na.rm = TRUE),
+                                            max_time = max(time_utc, na.rm = TRUE))
+
+# map the sites
+coords1 <- trim %>% distinct(latitude, longitude, dataset_id)
+mcoords <- st_as_sf(coords1, coords = c("longitude", "latitude"), crs = 4326)
+plot(mcoords$geometry)
+
+
