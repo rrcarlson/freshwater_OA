dba73f77a23ea8f151f16360e5c27df54683031a
rrcarlson <Rachel.R.Carl@gmail.com>
2023-02-20 16:09:02 -0800

    Revised filtering - debugged

--- EDA.R
+++ EDA.R
@@ -23,14 +23,20 @@ Scripps <- read.csv("/Users/rachelcarlson/Documents/Research/Postdoc-2022-presen
 variable.names(full)
 variable.names(Scripps)
 
+# Dataset 27 pH values are filtered out of the 'clean' dataset because they use YSI. However, this dataset will be important to us.
+# Filter out "clean" version of dataset 27 and then load the full (unfiltered) version back in.
+full <- full %>% filter(dataset_id != 27)
+CoastOA <- read.csv("/Users/rachelcarlson/Documents/Research/Postdoc-2022-present/Freshwater_time/planning/27_CoastalOA/27_final_coastal_OA.csv")
+
 # Bind datasets
 OOI$distance_offshore <- NA # Create a blank column for distance_offshore as this is missing in individual datasets
 Trinidad$distance_offshore <- NA
 Newport$distance_offshore <- NA
 Columbia$distance_offshore <- NA
 Scripps$distance_offshore <- NA
+CoastOA$distance_offshore <- NA
 
-full2 <- do.call("rbind", list(full, OOI, Trinidad, Newport, Columbia, Scripps)) # Bind datasets
+full2 <- do.call("rbind", list(full, OOI, Trinidad, Newport, Columbia, Scripps, CoastOA)) # Bind datasets
 
 # id_list <- c(30, 31, 32, 33, 39, 40, 50, 56, 64, 66, 67, 68, 70, 71)
 # trim <- full2 %>% filter(dataset_id %in% id_list)
@@ -52,7 +58,7 @@ full2 <- do.call("rbind", list(full, OOI, Trinidad, Newport, Columbia, Scripps))
 # 
 # st_write(mcoords,"/Users/rachelcarlson/Documents/Research/Postdoc-2022-present/Freshwater_time/planning/recommended_sites.9Feb2023.shp")
 
-# The above seems to be missing a lot - Recalculate time ranges and and plot full data
+# The above (only those datasets that were recommended specifically for this study) seems to be missing a lot - Recalculate time ranges and and plot full data
 full2 <- full2 %>% group_by(dataset_id) %>% mutate(min_time = min(time_utc, na.rm = TRUE),
                                                    max_time = max(time_utc, na.rm = TRUE))
 coords_full <- full2 %>% distinct(latitude, longitude, dataset_id, min_time, max_time) %>% na.omit()
@@ -71,19 +77,18 @@ st_write(mcoords_ts,"/Users/rachelcarlson/Documents/Research/Postdoc-2022-presen
 
 # I used a KML of the above to tag all sites located near river mouths. Need to filter sf object to this subset of sites.
 # Will use lat.lon to filter data so first need to remove tailing zeros from lat.lon decimals
-full2$latitude <- sub("0+$", "", as.character(full2$latitude))
-full2$longitude <- sub("0+$", "", as.character(full2$longitude))
+full2$latitude <- as.numeric(sub("0+$", "", as.character(full2$latitude))) %>% round(digits = 4)
+full2$longitude <- as.numeric(sub("0+$", "", as.character(full2$longitude))) %>% round(digits = 4)
 
 # Import sites tagged for river proximity
 fresh <- read.csv("/Users/rachelcarlson/Documents/Research/Postdoc-2022-present/Freshwater_time/planning/sites.csv")
-fresh$Lat <- sub("0+$", "", as.character(fresh$Lat))
-fresh$Lon <- sub("0+$", "", as.character(fresh$Lon))
+fresh$Lat <- as.numeric(sub("0+$", "", as.character(fresh$Lat))) %>% round(digits = 4)
+fresh$Lon <- as.numeric(sub("0+$", "", as.character(fresh$Lon))) %>% round(digits = 4)
 fresh <- fresh %>% dplyr::select(c("Site", "Dataset_ID","Distance","Reach_ID","Flow_range_midFeb","Mean_flow","Lat","Lon"))
 full2 <- full2 %>% rename("Lat" = "latitude", "Lon" = "longitude", "Dataset_ID" = "dataset_id") # There were 70ish mismatched dataset IDs in first try at joining these datasets. Ensure datest ID in tagged freshwater dataset is correct.
 
 # Join reach, site name, flow, and distance information to relevant sites
 full_fresh <- full2 %>% right_join(fresh, by = c("Lat" = "Lat", "Lon" = "Lon", "Dataset_ID" = "Dataset_ID"), multiple = "all")
-View(full_fresh %>% filter(Site == "Eel River and Humboldt Bay"))
 
 # Filter by sites with pH measured
 full_fresh_pH <- full_fresh %>% filter(!is.na(pH_total))
@@ -105,6 +110,7 @@ full_fresh_pH_temp <- full_fresh_pH %>%
 
 # Tally number of observations per Site with temp and pH data
 View(full_fresh_pH_temp %>% group_by(Site) %>% tally())
+View(full_fresh_4 %>% group_by(Site) %>% tally())
 View(full_fresh_3 %>% group_by(Site) %>% tally())
 
 # Plot pH v. temp. Departures should indicate areas where upwelling is not explanatory
@@ -186,7 +192,35 @@ full_fresh %>%
   ggplot(aes(x = sal_pss, y = ta_umolkg)) +
   geom_point()
 
-# Use Tomales Bay, Refugio, and SF Bay or Columbia River (if we can find data)
+# Columbia River
+
+full_fresh %>% 
+  filter(Site == "Columbia River") %>% 
+  ggplot(aes(x = t_C, y = pH_total)) +
+  geom_point()
+
+# small salinity - alkalinity trend
+full_fresh %>% 
+  filter(Site == "Columbia River") %>% 
+  ggplot(aes(x = sal_pss, y = ta_umolkg)) +
+  geom_point()
+
+# Tester
+
+full_fresh %>% 
+  filter(Site == "Ventura; Ventura River") %>% 
+  ggplot(aes(x = t_C, y = pH_total)) +
+  geom_point()
+
+# small salinity - alkalinity trend
+full_fresh %>% 
+  filter(Site == "Ventura; Ventura River") %>% 
+  ggplot(aes(x = sal_pss, y = ta_umolkg)) +
+  geom_point()
+
+# Use Tomales Bay, Refugio, and Columbia River
+# Other compelling cases: Umpqua River, Neskowin Creek, Rogue River, Tillamook Bay (Heitmiller Creek; Watseco Creek), Stillwater Cove (Stockhoff Creek), San Vicente Creek
+# Ventura has the opposite pattern (inverse TA/salinity), but barely
 
 # Add SF Bay to the mix. There are a TON of SF Bay sites so need to filter by lat/lon
 SF_Bay$Lat <- as.numeric(SF_Bay$Lat)
@@ -200,6 +234,18 @@ SF_Bay %>%
 
 sum(is.na(SF_Bay$ta_umolkg)) # Confirmed - NA values for TA (and pH) for the entire SF Bay dataset
 
+# Columbia River (offshore points)
+CR_lat <- c(46.857,46.9859,47.1336)
+CR_lon <- c(-124.244, -124.566, -124.272)
+
+CR <- full2 %>% filter(Lat %in% CR_lat) %>% filter(Lon %in% CR_lon)
+
+sum(!is.na(CR$pH_total)) # pH data is limited
+sum(!is.na(CR$t_C)) # temp data is more common
+
+View(CR[(!is.na(CR$pH_total)),]) # However times with pH data do not have temp (parameters do not overlap)
+
+CR_coast <- full_fresh %>% filter(Site == "Columbia River")
 
 
 
